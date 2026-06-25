62898dbb89575d13f7f9d7c640b20e89bbc36f19
Gold1705 <mkpascal02@gmail.com>
2026-06-25 12:26:53 +0000

    pushhh

--- num-manage-api/src/users/dto/create-user.dto.ts
+++ num-manage-api/src/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  nom!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  prenom!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsOptional()
+  @IsString()
+  telephone!: string;
+
+  @IsString()
+@IsNotEmpty()
+password!: string;
+}
--- num-manage-api/src/users/entities/user.entity.ts
+++ num-manage-api/src/users/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  nom!: string;
+
+  @Column()
+  prenom!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+   @Column({ nullable: true })
+password!: string;
+
+  @Column({ nullable: true })
+  telephone!: string;
+
+  @Column({ default: 'APPRENANT' })
+  role!: string;
+
+  @Column({ default: true })
+  estActif!: boolean;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+  @UpdateDateColumn()
+updatedAt!: Date;
+}
