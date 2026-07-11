6542e2c7e2b1f208843b5d406e81c992981d2e40
MrCLET <amlaganclement@gmail.com>
2026-07-11 12:54:37 +0000

    fix: corriger la faute de frappe roles.gard.ts en roles.guard.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,7 +17,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { RolesGuard } from '../common/guards/roles.gard';
+import { RolesGuard } from '../common/guards/roles.guard';
 import { Roles } from '../common/decorators/roles.decorator';
 import { Role } from '../common/enums/role.enum';
 
